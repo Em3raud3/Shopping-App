fef457b8b45c2f2dd9673ceebf34251030cd7782
Em3raud3 <animekamisama@gmail.com>
2022-06-01 15:17:26 -0400

    Section 3 (complete)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+    'This is simply a test',
+    'https://img.delicious.com.au/nnO4GCmJ/w759-h506-cfill/del/2015/10/baked-spinach-and-tomato-omelette-11971-1.jpg'),
+
+    new Recipe('A Test Recipe',
+    'This is simply a test',
+    'https://img.delicious.com.au/nnO4GCmJ/w759-h506-cfill/del/2015/10/baked-spinach-and-tomato-omelette-11971-1.jpg'),
+  ];
 
   constructor() { }
 

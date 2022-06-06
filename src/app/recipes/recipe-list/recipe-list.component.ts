0a1dc5224841a623bedc985a4c30423c7a228de8
Em3raud3 <animekamisama@gmail.com>
2022-06-06 16:24:49 -0400

    88

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe',
     'This is simply a test',
     'https://img.delicious.com.au/nnO4GCmJ/w759-h506-cfill/del/2015/10/baked-spinach-and-tomato-omelette-11971-1.jpg'),
 
-    new Recipe('A Test Recipe',
+    new Recipe('Another Test Recipe',
     'This is simply a test',
     'https://img.delicious.com.au/nnO4GCmJ/w759-h506-cfill/del/2015/10/baked-spinach-and-tomato-omelette-11971-1.jpg'),
   ];
@@ -20,6 +21,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }

2b64d4755cb70cb803c1816df52f5c1e13c475be
Em3raud3 <animekamisama@gmail.com>
2022-06-07 16:33:04 -0400

    103

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpened = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpened = !this.isOpened;
+  }
+
+  constructor() { }
+
+}
